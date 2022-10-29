b6173b9d757089484c73335ed937e17edc07cd5b
Rafał Konieczny <rkonieczny87@gmail.com>
2022-10-30 00:57:46 +0200

    Completed exercise - Objects

---
+++ Objects/solution.js
@@ -0,0 +1,14 @@
+let car1 = {
+    Mark: "Honda",
+    Model: "Civic",
+    productionYear: 1995
+};
+
+let car2 = {
+    Mark: "Skoda",
+    Model: "Kamiq",
+    productionYear: 2021
+};
+
+console.log(car1);
+console.log(car2);
